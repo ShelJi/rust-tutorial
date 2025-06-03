45b12d42db2bbcd9e19ec7fd851ffb3b485fba49
Sheljin S.A <shelj.dev@gmail.com>
2025-06-03 23:48:43 +0530

    variable  shadowing scopes

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -4,14 +4,21 @@ const ONE_MINUTE: i32 = 60;
 
 fn main() {
     // Immutable variable (default)
+    println!("------------------");
+    println!("IMMuttable in Rust");
+    println!("------------------");
+    
     let x = 5;
     println!("The value of x is: {}", x);
     // x = 6; // ❌ This would cause a compile-time error
     println!("The value of x is: {}", x);
 
+    
+    // Muttable variable
+    println!("------------------");
+    println!("Muttable in Rust");
     println!("------------------");
 
-    // Muttable variable
     // Shadowing x with a new mutable variable
     let mut x = 5;
     println!("The value of x is: {}", x);
@@ -20,6 +27,8 @@ fn main() {
     println!("The value of x is: {x}");
 
     println!("------------------");
+    println!("Constants in Rust");
+    println!("------------------");
 
     // Constant variable
     // i32 -> integer of 32 bits
@@ -27,4 +36,16 @@ fn main() {
     const ONE_HOUR: i32 = ONE_MINUTE * 60;
 
     println!("one hour is {ONE_HOUR} seconds");
+
+    // Variable shadowing
+        println!("------------------");
+    println!("Variable shadowing in Rust");
+    println!("------------------");
+    let n = 1;
+    {
+        let n = 2;
+        println!("The value of n in the inner scope is: {}", n);
+    }
+
+    println!("The value of n in the outer scope is: {}", n);
 }
