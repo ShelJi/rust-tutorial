b82d50382aa5553be21980021a56225613319463
Sheljin S.A <shelj.dev@gmail.com>
2025-06-03 23:50:58 +0530

    inner variable

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -7,13 +7,12 @@ fn main() {
     println!("------------------");
     println!("IMMuttable in Rust");
     println!("------------------");
-    
+
     let x = 5;
     println!("The value of x is: {}", x);
     // x = 6; // ❌ This would cause a compile-time error
     println!("The value of x is: {}", x);
 
-    
     // Muttable variable
     println!("------------------");
     println!("Muttable in Rust");
@@ -38,7 +37,7 @@ fn main() {
     println!("one hour is {ONE_HOUR} seconds");
 
     // Variable shadowing
-        println!("------------------");
+    println!("------------------");
     println!("Variable shadowing in Rust");
     println!("------------------");
     let n = 1;
@@ -46,6 +45,10 @@ fn main() {
         let n = 2;
         println!("The value of n in the inner scope is: {}", n);
     }
-
+    
     println!("The value of n in the outer scope is: {}", n);
+    
+    {
+        println!("The value of n in the inner scope without declaring is: {}", n);
+    }
 }
